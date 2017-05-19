edade793dbb0f048d16081d3c6e4c02ab96cfb12
Oscar Levin <oscar.levin@gmail.com>
2017-05-19 12:46:48 -0600

    copied develop branch from discrete-book

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# dmoi-instructor
+discrete-book
+=============
+
+An open textbook for Discrete Mathematics, as taught at the University of Northern Colorado.  More information about the project is available on [the book's main website](http://discretetext.oscarlevin.com).
+
+## Fall 2015 corrected edition
+
+This current branch (Fall2015-corrected) contains the source files for the corrected version of the Fall 2015 edition.  Use this in case you have that edition or want to continue using that edition.  Most people will want to wait for the Fall 2016 edition, which will be released in early August.  For progress on this, see the development branch.
+
+## Compilation Instructions
+
+To compile, compile the file "discretebook.tex" using pdfLaTeX or similar.  If you would prefer to have margins more suitable for printing and binding (specifically two-sided), compile "discretebook-print.tex"
+
+In case you would like to grab just single chapters or exercise sets, you *should* be able to compile each of those idividually as well (thanks to the DocMute package).
+
+## Contributing
+
+Any and all suggestions to improve the text are welcome.  Thanks to those who have already pointed out typos/issues they have found.  If you would like to make a more substantial contribution, please contact me so we can discuss how best to proceed.
+
+## ToDo
+
+Over the next few months (May to August 2016) I plan to convert the project to MathBookXML as well as make other improvements.  A new edition will be available for the Fall 2016 semester.
---
+++ html/images/home.php
@@ -0,0 +1,53 @@
+<?php 
+$title = "Oscar Levin's Math Page";
+$body_id = "home";
+include('common/head.php');
+?>
+
+<div id="content">
+
+<h1>Welcome</h1>
+
+<h2>Coordinates:</h2>
+<p><a class='plain' href="http://www.unco.edu/nhs/mathematical-sciences/">School of Mathematical Sciences</a><br/>
+<a class='plain' href="http://www.unco.edu">University of Northern Colorado</a>
+<br/>
+Greeley, CO  80639 </p>
+
+
+<p>
+<b>Office:</b> Ross 2239-G<br/>
+<b>Phone:</b> 970-351-2380 <br/>
+<b>Email:</b> <a
+ href="mailto:oscar.levin@unco.edu">oscar.levin@unco.edu</a>
+</p>
+
+<br/>
+
+
+<h2>Discrete Textbook</h2>
+
+<p><a href="discretetext">Discrete Mathematics: An Open Introduction</a> is now available to download for free or purchase for cheap.  More information about this open source textbook can be found by clicking the link above.</p>
+
+
+<h2>Teaching: </h2>
+<p>This Spring (2016) I am teaching Supplemental Calculus (Math 130), Discrete Mathematics (Math 228) and  Abstrac Algebra II (Math 322).  Course materials will be available through <a href="http://bb.unco.edu" target="_blank">Blackboard</a>.
+
+
+
+<p>I have begun recording videos in which I work out some example problems.  These can be seen on my YouTube channel: <a href="http://www.youtube.com/user/oscarlevin" target="_blank">Mathematics Exemplified</a>.</p> 
+
+<p>Since arriving at UNC, I taught Discrete Mathematics, Modern Geometry I, Abstract Algebra I and II, Calculus I and II and Topics in Calculus, as well as graduate courses in Logic, Graph Theory, Analysis, and Probability and Statistics.  I have taught a wide variety of courses at Coastal Carolina University and the University of Connecticut as well.  Course websites for most of those classes are available <a href="teaching.php">here</a>.</p>
+
+<h2>Research</h2>
+<p>My primary interests lie in effective algebra and combinatorics, computability theory, reverse mathematics, and mathematical logic.  Effective algebra endeavors to understand results of algebra and combinatorics from a viewpoint of computability theory.  My Ph.D. dissertation concerns the effective content of ordered fields.  My thesis, along with some other papers I've worked on, are available on my <a href="research.php">research page</a></p>
+
+<h2>Mathematical Puzzles</h2>
+<p>I enjoy using mathematical puzzles in teaching.  I have started a collection of the best of these puzzles on what I'm calling the <a href="http://mathpuzzlewiki.com" target="_blank">Math Puzzle Wiki</a>.  If you are a math teacher looking for a good extra credit problem, or simply a puzzle enthusiast, please check it out.  You can even contribute your own puzzles or add to ones already there.
+</p>
+
+
+</div>
+
+
+<?php include('common/foot.php') ?>
